532d6ed5a616f0d1d7710fe4c5f4278053d13278
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-13 12:05:44 +0300

    Add files via upload

---
+++ DaireAlan.java
@@ -0,0 +1,21 @@
+package Baslangic;
+import java.util.Scanner;
+public class DaireAlan {
+    public static void main(String[] args) {
+        int r;
+        double alan, cevre, a, pi = 3.14, dilimAlan, dilimCevre;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Dairenin yarı çapı : ");
+        r = input.nextInt();
+        System.out.print("Daire diliminin açısını girin : ");
+        a = input.nextInt();
+        alan = pi * r * r;
+        cevre = 2 * pi * r;
+        dilimAlan = (pi * r * r * a) / 360;
+        dilimCevre = 2 * r + (pi * r * r )/360;
+        System.out.println("Yarıçapı girilen dairenin çevresi " + cevre + "ve alanı " + alan);
+        System.out.println("Açısı girilen daire diliminin alanı " + dilimAlan);
+        System.out.println("Açısı girilen daire diliminin çevresi " + dilimCevre);
+    }
+}
